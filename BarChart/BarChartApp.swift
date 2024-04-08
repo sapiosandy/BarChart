e789ef7fd395d227cc411bdd5c35e1d6dce4cdaf
Sandy Gomez <sgomez1029@gmail.com>
2024-04-08 11:12:17 -0700

    Initial Commit

---
+++ BarChart/BarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartApp.swift
+//  BarChart
+//
+//  Created by Sandra Gomez on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
