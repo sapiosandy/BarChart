c97f0aee6f12dbc65d2f6c13d5562b7393896e1b
Sandy Gomez <sgomez1029@gmail.com>
2024-04-09 21:20:58 -0700

    Lecture 1: Lesson 2 (UCSD Futures iOS)

--- BarChart/ContentView.swift
+++ BarChart/ContentView.swift
@@ -5,19 +5,32 @@
 //  Created by Sandra Gomez on 4/8/24.
 //
 
+import Charts
 import SwiftUI
+
+struct SalesPeople: Identifiable {
+    var name: String
+    var sales: Int
+    var id: String { name }
+}
+
+var mySalesArray: [SalesPeople] = [
+    .init(name: "Oscar", sales: 50),
+    .init(name: "Mayer", sales: 43),
+    .init(name: "George", sales: 62),
+    .init(name: "Hormel", sales: 26)
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart (mySalesArray){ z in
+            BarMark(x: .value("Name", z.name),
+                    y: .value("Sales", z.sales))
         }
-        .padding()
+        
     }
 }
+
 
 #Preview {
     ContentView()
